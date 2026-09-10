df78ac5d31110977e3baca3351ecbf1e7f4d9651
Avradeep Mondal <avradeepmondal10@gmail.com>
2026-09-10 12:26:30 +0530

    Added method to find latest completed attemp

--- src/main/java/com/avradeep/AttemptService/repository/QuizAttemptRepository.java
+++ src/main/java/com/avradeep/AttemptService/repository/QuizAttemptRepository.java
@@ -4,14 +4,23 @@ import com.avradeep.AttemptService.entity.AttemptStatus;
 import com.avradeep.AttemptService.entity.QuizAttempt;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface QuizAttemptRepository
         extends MongoRepository<QuizAttempt, String> {
 
+    // Find currently active attempt
     Optional<QuizAttempt> findTopByUserIdAndQuizIdAndStatusOrderByStartTimeDesc(
             String userId,
             String quizId,
             AttemptStatus status
+    );
+
+    // Find latest completed attempt
+    Optional<QuizAttempt> findTopByUserIdAndQuizIdAndStatusInOrderBySubmittedAtDesc(
+            String userId,
+            String quizId,
+            Collection<AttemptStatus> statuses
     );
 }
