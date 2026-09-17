e2d6694bdae458f926fd58c6fc16021cf260a5e4
Avradeep Mondal <avradeepmondal10@gmail.com>
2026-09-17 09:58:30 +0530

    Modified with adding JWT token propagation with Feign

--- src/main/java/com/avradeep/AttemptService/cleint/QuestionClient.java
+++ src/main/java/com/avradeep/AttemptService/cleint/QuestionClient.java
@@ -1,13 +1,17 @@
 package com.avradeep.AttemptService.cleint;
 
 import com.avradeep.AttemptService.dto.QuestionEvaluationResponse;
+import com.avradeep.AttemptService.security.FeignTokenPropagationConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "QUESTION-SERVICE")
+@FeignClient(
+        name = "QUESTION-SERVICE",
+        configuration = FeignTokenPropagationConfig.class
+)
 public interface QuestionClient {
 
     @GetMapping("/api/questions/{quizId}/evaluation")
